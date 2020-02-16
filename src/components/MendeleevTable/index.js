cf2afb8032ff86af531eebb0f3cc290d677e3553
alexandrtovmach <alexandrtovmach@gmail.com>
2020-02-16 15:40:15 +0200

    feat: Mapped keys data

--- src/components/MendeleevTable/index.js
+++ src/components/MendeleevTable/index.js
@@ -1,15 +1,14 @@
 import React from "react"
 
-import { page1 } from "../../data/table.json"
+import tableData from "../../data/table.json"
 import { Element } from "../"
 import styles from "./table.module.scss"
 
 const Table = () => {
-  console.log(page1);
   return (
     <div className={styles.tableContainer}>
       {
-        page1.map((elementData) => (
+        tableData.map((elementData) => (
           <Element elData={elementData} />
         ))
       }
